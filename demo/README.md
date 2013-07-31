f73aca4e2710e68a7e60a2cb1396f5d6e11d727f
Shibbir Ahmed <shibbir.io@outlook.com>
2013-08-01 03:05:16 +0600

    ui modified for github page

---
+++ demo/README.md
@@ -0,0 +1,11 @@
+SimpleContactManager
+====================
+
+A super simple HTML5 based contact manager using html5 indexedDb
+
+
+1. [Twitter Bootstrap](http://twbs.github.io/bootstrap/)
+2. [Glyphicons](http://glyphicons.getbootstrap.com/)
+3. [IDBWrapper](http://jensarps.github.io/IDBWrapper/)
+4. [Handlebars](http://handlebarsjs.com/)
+5. [Parsley](http://parsleyjs.org/)
---
+++ demo/js/app/contact-repository.js
@@ -0,0 +1,94 @@
+var ContactRepository = function () {};
+
+ContactRepository.prototype = function () {
+	var contacts, contactNodeCache = {};
+
+	var initContactStore = function () {
+		contacts = new IDBStore({
+			storeName: "contact",
+			dbVersion: 1,
+  			keyPath: 'contactid',
+			autoIncrement: true,
+			onStoreReady: refreshContacts
+		});
+
+		["addContact", "FirstName", "LastName", "Mobile", "Phone", "Email"].forEach(function(id) {
+			contactNodeCache[id] = document.getElementById(id);
+		});
+
+		contactNodeCache.addContact.addEventListener("click", createContact);
+	};
+	var refreshContacts = function () {
+		contacts.getAll(contactList);
+	};
+
+	var contactList = function (data) {
+		var groupArray = [];
+		$("#placeholder-groups table tbody tr").each(function () {
+		    var $this = $(this);
+		    groupArray[$this.data("groupId")] = $this.data("groupName");
+		});
+
+		data.forEach(function (contact) {
+			contact.GroupName = groupArray[contact.Group];
+		});
+
+	 	var source = $("#template-contacts").html();
+		template = Handlebars.compile(source);
+		$("#placeholder-contacts").html(template({
+			contacts: data
+		}));
+	};
+
+	var createContact = function () {
+		var contactAddForm = $("#contact-add-form");
+		contactAddForm.parsley("validate");
+
+		if(contactAddForm.parsley("isValid")) {
+			var data = {};
+			["FirstName", "LastName", "Mobile", "Phone", "Email", "Group"].forEach(function(key) {
+				if(key === "Group") {
+					var value = document.getElementById(key).value.trim();
+				}
+				else {
+					var value = contactNodeCache[key].value.trim();
+				}
+				data[key] = value;
+			});
+			contacts.put(data, function() {
+				$("input").val("");
+				refreshContacts();
+				$(".notification-add-contact").html('<div class="alert alert-success"><span>New record created.</span></div>').fadeIn(200).delay(1500).fadeOut(300);
+			});
+		}
+	};
+
+	var editContact = function (contact) {
+		var data = {
+			contactid: parseInt(contact.Id, 10),
+			FirstName: contact.FirstName.trim(),
+			LastName: contact.LastName.trim(),
+			Phone: contact.Phone.trim(),
+			Mobile: contact.Mobile.trim(),
+			Email: contact.Email.trim(),
+			Group: contact.Group.trim()
+		};
+
+		var contactEditForm = $("#contact-edit-form");
+		contactEditForm.parsley("validate");
+
+		if(contactEditForm.parsley("isValid")) {
+			contacts.put(data, refreshContacts);
+			$(".notification-edit-contact").html('<div class="alert alert-success"><span>The record has been updated.</span></div>').fadeIn(200).delay(1500).fadeOut(300);
+		}
+	};
+
+	var deleteContact = function (id) {
+		contacts.remove(id, refreshContacts);
+	};
+	return {
+		initContactStore: initContactStore,
+		editContact: editContact,
+		deleteContact: deleteContact
+	};
+}();
